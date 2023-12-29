2db20a3a3fd4de4f1f68207198932c5c8137725f
Vaanee Tripathi <124565270+VaaneeTripathi@users.noreply.github.com>
2023-12-29 13:25:04 +0530

    Restructure repository

---
+++ arrays/searches/binarysearch.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+// Objective: perform binary search on a sorted array to find a given element
+
+
+
+// check the middle element
+// if match, return true
+// if less call function on left
+// if more call function right half
